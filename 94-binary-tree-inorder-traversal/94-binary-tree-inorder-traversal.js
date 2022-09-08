d24395567fa55d07e0b571fe9147964ed24cae68
Pedro Spoljaric <pedrospoljaric@gmail.com>
2022-09-08 19:09:35 -0300

    Time: 64 ms (92.10%), Space: 42.2 MB (43.95%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const values = []
+    const inOrder = (node) => {
+        if (!node) return
+        inOrder(node.left)
+        values.push(node.val)
+        inOrder(node.right)
+    }
+    inOrder(root)
+    return values
+};
